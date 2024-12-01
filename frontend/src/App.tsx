fbe35b20e9c88b8d547b4881115b60448fab991b
Sunil0881 <sunil12162003@gmail.com>
2024-12-01 15:13:43 +0530

    react init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-red-500 font-bold text-3xl'>
+    we will make it !
+    </div>
+ 
+    </>
+  )
+}
+
+export default App
